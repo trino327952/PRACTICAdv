c0d567a06c410a27cd0d7deda12838f54e785768
Cinthya Alvarez <cinthya.alvarez@espam.edu.ec>
2026-08-08 10:45:46 -0500

    fix: agregar configuracion eslintrc.cjs en frontend

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  ignorePatterns: ['dist', 'node_modules', '.eslintrc.cjs'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-unused-vars': 'warn',
+  },
+};
