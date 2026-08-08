c4f5a8220a82dac19c4402e415d30bd61712cff2
Cinthya Alvarez <cinthya.alvarez@espam.edu.ec>
2026-08-08 11:55:33 -0500

    fix: guardar cambios de frontend y eslintrc

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react-hooks/set-state-in-effect': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+  },
+};
